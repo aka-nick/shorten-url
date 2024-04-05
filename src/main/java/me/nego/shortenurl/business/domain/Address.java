92d93d87ac9224b80a57073a18e1ba1c9ee5be5a
JeongSu Park <bechalrang@gmail.com>
2024-04-05 17:44:22 +0900

    Address: 최초 작성

---
+++ src/main/java/me/nego/shortenurl/business/domain/Address.java
@@ -0,0 +1,12 @@
+package me.nego.shortenurl.business.domain;
+
+/**
+ * @author Nick
+ * @since 2024-04-05 17:43
+ */
+public record Address(
+        String original,
+        String shortened
+) {
+
+}
