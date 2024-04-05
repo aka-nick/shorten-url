ba52e6a75819234860b2ecca9f57167b67a3178d
JeongSu Park <bechalrang@gmail.com>
2024-04-05 17:36:33 +0900

    AddressRepository: 최초 작성

---
+++ src/main/java/me/nego/shortenurl/infrastructure/repository/AddressRepository.java
@@ -0,0 +1,15 @@
+package me.nego.shortenurl.infrastructure.repository;
+
+import java.util.Optional;
+import me.nego.shortenurl.infrastructure.entity.AddressJpaEntity;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+/**
+ * @author Nick
+ * @since 2024-04-05 17:36
+ */
+public interface AddressRepository extends JpaRepository<AddressJpaEntity, Long> {
+
+    Optional<AddressJpaEntity> findByOriginal(String original);
+
+}
