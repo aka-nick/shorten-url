51f7a1d20a459e078df044d29262b707e91db2e4
JeongSu Park <bechalrang@gmail.com>
2024-04-06 08:46:08 +0900

    ShortenScenario#makeShortenByOriginal: 단축 생성 기능 추가

--- src/main/java/me/nego/shortenurl/business/scenario/ShortenScenario.java
+++ src/main/java/me/nego/shortenurl/business/scenario/ShortenScenario.java
@@ -1,8 +1,11 @@
 package me.nego.shortenurl.business.scenario;
 
+import java.time.LocalDateTime;
 import java.util.Optional;
 import me.nego.shortenurl.business.model.Address;
+import me.nego.shortenurl.common.util.HashDigestor;
 import me.nego.shortenurl.infrastructure.entity.AddressJpaEntity;
+import me.nego.shortenurl.infrastructure.service.AddressCommand;
 import me.nego.shortenurl.infrastructure.service.AddressQuery;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
@@ -15,10 +18,18 @@ import org.springframework.transaction.annotation.Transactional;
 @Transactional(readOnly = true)
 public class ShortenScenario {
 
-    private final AddressQuery addressQuery;
+    private static final int SHORTEN_LENGTH = 5;
+    private static final long EXPIRE_TERM = 7L;
 
-    public ShortenScenario(AddressQuery addressQuery) {
+    private final AddressQuery addressQuery;
+    private final AddressCommand addressCommand;
+
+    public ShortenScenario(
+            AddressQuery addressQuery,
+            AddressCommand addressCommand
+    ) {
         this.addressQuery = addressQuery;
+        this.addressCommand = addressCommand;
     }
 
     public Optional<Address> retrieveAddressByOriginal(String original) {
@@ -32,6 +43,22 @@ public class ShortenScenario {
                 found.getOriginal(),
                 found.getShortened()
         ));
+    }
+
+    public Address makeShortenByOriginal(String original) {
+        String hashed = HashDigestor.hash(original);
+        String shortened = hashed.substring(0, SHORTEN_LENGTH);
+
+        AddressJpaEntity save = addressCommand.save(new AddressJpaEntity(
+                original,
+                shortened,
+                LocalDateTime.now().plusDays(EXPIRE_TERM)
+        ));
+
+        return new Address(
+                save.getOriginal(),
+                save.getShortened()
+        );
     }
 
 }
