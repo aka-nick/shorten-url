b032b4fe30485518aba158a04e66b5907aa4d404
JeongSu Park <bechalrang@gmail.com>
2024-04-05 17:37:28 +0900

    AddressQuery: 최초 작성

---
+++ src/main/java/me/nego/shortenurl/infrastructure/service/AddressQuery.java
@@ -0,0 +1,27 @@
+package me.nego.shortenurl.infrastructure.service;
+
+import java.util.Optional;
+import me.nego.shortenurl.infrastructure.entity.AddressJpaEntity;
+import me.nego.shortenurl.infrastructure.repository.AddressRepository;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+/**
+ * @author Nick
+ * @since 2024-04-05 17:37
+ */
+@Service
+@Transactional(readOnly = true)
+public class AddressQuery {
+
+    private final AddressRepository addressRepository;
+
+    public AddressQuery(AddressRepository addressRepository) {
+        this.addressRepository = addressRepository;
+    }
+
+    public Optional<AddressJpaEntity> queryByOriginal(String original) {
+        return addressRepository.findByOriginal(original);
+    }
+
+}
