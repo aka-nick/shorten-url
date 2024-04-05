aaf2daaf17b005f4816a1feb9c1a3802afd10f5f
JeongSu Park <bechalrang@gmail.com>
2024-04-05 18:07:25 +0900

    AddressJpaEntity: 미사용 import 제거

--- src/main/java/me/nego/shortenurl/infrastructure/entity/AddressJpaEntity.java
+++ src/main/java/me/nego/shortenurl/infrastructure/entity/AddressJpaEntity.java
@@ -6,7 +6,6 @@ import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
 import jakarta.persistence.EntityListeners;
 import jakarta.persistence.GeneratedValue;
-import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
 import java.time.LocalDateTime;
 import org.springframework.data.annotation.CreatedDate;
