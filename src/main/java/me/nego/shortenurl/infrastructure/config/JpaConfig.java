c366956c42865a3d6f12ed01d607808d5fcc856a
JeongSu Park <bechalrang@gmail.com>
2024-04-05 18:08:22 +0900

    JpaConfig: 누락한 createdBy 주석 추가

--- src/main/java/me/nego/shortenurl/infrastructure/config/JpaConfig.java
+++ src/main/java/me/nego/shortenurl/infrastructure/config/JpaConfig.java
@@ -5,6 +5,10 @@ import org.springframework.context.annotation.Configuration;
 import org.springframework.data.jpa.domain.support.AuditingEntityListener;
 import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 
+/**
+ * @author Nick
+ * @since 2024-04-05 18:07
+ */
 @Configuration
 @EnableJpaAuditing
 @EntityListeners(AuditingEntityListener.class)
