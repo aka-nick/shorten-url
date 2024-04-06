2c7aa652dd707336b5b5d8fb2d7d68abaf0aef20
JeongSu Park <bechalrang@gmail.com>
2024-04-06 12:25:29 +0900

    AddressRepository: shortened로 original 값을 조회해오는 메서드의 시그니처 추가

--- src/main/java/me/nego/shortenurl/infrastructure/repository/AddressRepository.java
+++ src/main/java/me/nego/shortenurl/infrastructure/repository/AddressRepository.java
@@ -12,4 +12,6 @@ public interface AddressRepository extends JpaRepository<AddressJpaEntity, Long>
 
     Optional<AddressJpaEntity> findByOriginal(String original);
 
+    AddressJpaEntity findByShortened(String shortened);
+
 }
