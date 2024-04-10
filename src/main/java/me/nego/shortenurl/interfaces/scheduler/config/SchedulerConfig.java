58825fcab00081fee15b2b3ab28e473d7d2e51f7
JeongSu Park <bechalrang@gmail.com>
2024-04-10 09:19:53 +0900

    SchedulerConfig: 스케줄링 활성화 설정 추가

---
+++ src/main/java/me/nego/shortenurl/interfaces/scheduler/config/SchedulerConfig.java
@@ -0,0 +1,15 @@
+package me.nego.shortenurl.interfaces.scheduler.config;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.scheduling.annotation.EnableScheduling;
+
+/**
+ * @author Nick
+ * @since 2024-04-10
+ */
+
+@Configuration
+@EnableScheduling
+public class SchedulerConfig {
+
+}
