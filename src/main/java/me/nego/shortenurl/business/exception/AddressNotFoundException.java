9d288aeb94baa2550aa615ddf4e5be531c655268
JeongSu Park <bechalrang@gmail.com>
2024-04-11 18:17:21 +0900

    AddressNotFoundException: 빠뜨린 작성자/작성일시 주석 추가

--- src/main/java/me/nego/shortenurl/business/exception/AddressNotFoundException.java
+++ src/main/java/me/nego/shortenurl/business/exception/AddressNotFoundException.java
@@ -1,5 +1,10 @@
 package me.nego.shortenurl.business.exception;
 
+/**
+ * @author Nick
+ * @since 2024-04-11
+ */
+
 public class AddressNotFoundException extends RuntimeException {
 
     private static final String EXCEPTION_MESSAGE_FORMAT = "유효하지 않은 단축 URL입니다 (%s)";
